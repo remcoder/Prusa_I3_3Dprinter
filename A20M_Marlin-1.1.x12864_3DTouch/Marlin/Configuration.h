0d966b983a749a37666e1df44257347d476ca6d2
Remco Veldkamp <remcoder@gmail.com>
2020-02-02 20:38:06 +0100

    Invert the steppers for the tmc2208 drivers

--- A20M_Marlin-1.1.x12864_3DTouch/Marlin/Configuration.h
+++ A20M_Marlin-1.1.x12864_3DTouch/Marlin/Configuration.h
@@ -766,9 +766,9 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR  true//true
-#define INVERT_Y_DIR  true
-#define INVERT_Z_DIR  false//true
+#define INVERT_X_DIR  false // for tmc2208
+#define INVERT_Y_DIR  false // for tmc2208
+#define INVERT_Z_DIR  false // unchanged a4988
 
 // Enable this option for Toshiba stepper drivers
 //#define CONFIG_STEPPERS_TOSHIBA
@@ -776,8 +776,8 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
-#define INVERT_E1_DIR false
+#define INVERT_E0_DIR true // for tmc2208
+#define INVERT_E1_DIR true // for tmc2208
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
 #define INVERT_E4_DIR false
