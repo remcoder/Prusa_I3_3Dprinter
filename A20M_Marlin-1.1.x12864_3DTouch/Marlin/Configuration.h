1dc4f215a76b8c6ab92a165c728b8c23071f65e0
Remco Veldkamp <remcoder@gmail.com>
2021-02-21 12:01:55 +0100

    update esteps per Hemera docs

--- A20M_Marlin-1.1.x12864_3DTouch/Marlin/Configuration.h
+++ A20M_Marlin-1.1.x12864_3DTouch/Marlin/Configuration.h
@@ -549,7 +549,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.8, 400, 340 }   //Orig { 80, 80, 4000, 500 } 93*2 = DRV8825 1/32 Stepps
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.8, 400, 409 }   //Orig { 80, 80, 4000, 500 } 93*2 = DRV8825 1/32 Stepps
 
 /**
  * Default Max Feed Rate (mm/s)
